b04e68d678a3b6a69cd87bebc26ba781f71e026d
903774 <bakkerjangert@hotmail.com>
2023-12-06 09:06:32 +0100

    Day 6

--- Day 06/Day 06.py
+++ Day 06/Day 06.py
@@ -10,11 +10,10 @@ distances = list(map(int, data[1].split(':')[1].split()))
 times.append(int(''.join((map(str, times)))))
 distances.append(int(''.join(map(str, distances))))
 
-pt1, pt2 = 1, 0
-for i, t in enumerate(times):
-    d = distances[i]
+pt1, pt2 = 1, 1
+for d, t in zip(distances, times):
     seconds = ((t + (t ** 2 - 4 * d) ** 0.5) / 2, (t - (t ** 2 - 4 * d) ** 0.5) / 2)
-    s1, s2 = int(min(seconds)), int(max(seconds))
+    s1, s2 = int(min(seconds)), int(max(seconds))  # int truncates number; add 1 for s1, s2 should be correct
     # account for edge case that sqrt() result in int number
     while s1 * (t - s1) <= d:
         s1 += 1
@@ -23,8 +22,8 @@ for i, t in enumerate(times):
     if t != times[-1]:
         pt1 *= (s2 - s1 + 1)
     else:
-        pt2 = (s2 - s1 + 1)
+        pt2 *= (s2 - s1 + 1)
 print(f'Part 1: {pt1}')
 print(f'Part 2: {pt2}')
 
-# d = s * (t - s) --> d = st - s2 --> s2 - st + d = 0 --> Use ABC formula to get s
+# d = s * (t - s) --> d = st - s2 --> s2 - st + d = 0 --> Use ABC formula to get s; see var seconds above
