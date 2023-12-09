d68a4f5df4bd692c7c2f56ea79ae178e870d1b98
903774 <bakkerjangert@hotmail.com>
2023-12-09 06:32:21 +0100

    Day 9

--- Day 09/Day 09.py
+++ Day 09/Day 09.py
@@ -8,16 +8,12 @@ with open(file, 'r') as f:
 pt1, pt2 = 0, 0
 for line in data:
     row = [list(map(int, line.split()))]
-    while True:
+    next_delta = [999]
+    while next_delta.count(0) != len(next_delta):
         next_delta = []
-        stop = True
         for i, j in zip(row[-1][:-1], row[-1][1:]):
             next_delta.append(j - i)
-            if next_delta[-1] != 0:
-                stop = False
         row.append(next_delta)
-        if stop:
-            break
     row_solution_pt1, row_solution_pt2 = 0, 0
     for i in row[::-1]:
         row_solution_pt1 += i[-1]
