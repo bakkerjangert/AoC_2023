bd54c32b2f88de7b2d7477c6adb66e286cfafe61
903774 <bakkerjangert@hotmail.com>
2023-12-15 07:44:40 +0100

    Day 15

--- Day 15/Day 15.py
+++ Day 15/Day 15.py
@@ -9,19 +9,21 @@ file = 'input.txt'
 with open(file, 'r') as f:
     data = f.read().splitlines()
 
-boxes = {i: dict() for i in range(256)}
+boxes = {i: dict() for i in range(256)}  # use fact that dicts are ordered now in python
 pt1, pt2 = 0, 0
+
 for string in data[0].split(','):
     pt1 += get_val(string)
-    index = max(string.find('='), string.find('-'))
+    index = max(string.find('='), string.find('-'))  # use fact that if character not found returned index = -1
     name, box = string[:index], get_val(string[:index])
     if '=' in string:
-        boxes[box][name] = int(string[index + 1:])
-    elif '-' in string:
-        if name in boxes[box]:
-            del boxes[box][name]
+        boxes[box][name] = int(string[index + 1:])  # replaced if key exists, otherwise added at bottom
+    elif '-' in string and name in boxes[box]:
+        del boxes[box][name]  # neat that dicts are ordered now in python
+
 for box in boxes:
     for pos, lens in enumerate(boxes[box]):
         pt2 += (box + 1) * (pos + 1) * boxes[box][lens]
+
 print(f'Part 1: {pt1}')
 print(f'Part 2: {pt2}')
